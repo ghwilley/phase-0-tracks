97692ebb4b122e045426093f1f32825b8a17dfa5
Grant Willey <ghwilley@gmail.com>
2016-03-21 19:20:15 -0500

    Started modding vamp file

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+puts 'What is your name?'
+name = gets.chomp
+
+puts 'How old are you?'
+age = gets.chomp
+
+puts 'What year were you born?'
+birth_year = gets.chomp
+
+puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
+garlic = gets.chomp
+
+puts 'Would you like to enroll in the company\'s health insurance?'
+insurance = gets.chomp
+
