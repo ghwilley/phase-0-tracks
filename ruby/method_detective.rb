819ffee31a457ae9b780443d1bfdf9142f79cdce
Grant Willey <ghwilley@gmail.com>
2016-03-28 16:19:43 -0500

    completed 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+p "iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN”
 
-# “zom”.<???>
-# => “zoom”
+p "zom".insert(1 , "o")
+# => "zoom”
 
-# “enhance”.<???>
-#=> “    enhance    ”
+p "enhance".center(15)
+p "enhance".rjust(11, " ").ljust(15, " ")
+#=> "    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
-#=> “the usual suspects”
+p "Stop! You're under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!”
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+p "the usual".concat(" suspects")
+#=> "the usual suspects”
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+p " suspects".prepend("the usual")
+#=> "the usual suspects”
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+p "The case of the disappearing last letter".chomp('r')
+p "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+p "The mystery of the missing first letter".slice(1..38)
+# => "he mystery of the missing first letter”
 
-#“z”.<???>
+p "Elementary  ,    my   dear      Watson!".squeeze(' ')
+# => Elementary, my dear Watson!”
+
+p "z".sum
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (That is where z falls in line of all characters that can be understood
+# from what I've found it starts with special characters, then caps, then lowercase)
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+p "How many times does the letter 'a' appear in this string?".count("a")
 #=> 4
