d2bec9c9b57ca910f0cd5a19d65ebbdf288cac1d
Grant Willey <ghwilley@gmail.com>
2016-03-31 12:03:33 -0500

    added interface

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -42,21 +42,30 @@ end
 puts "
 	Name: #{user_info[:name]}
 	Age: #{user_info[:age]}
-	Number of kids: #{user_info[:num_kids]}
-	Decor Theme: #{user_info[:decor]}
-	Color Preference: #{user_info[:colors]}
-	Number of Windows: #{user_info[:num_windows]}
-	Fireplace present: #{user_info[:fireplace]}
+	Kids: #{user_info[:num_kids]}
+	Decor: #{user_info[:decor]}
+	Colors: #{user_info[:colors]}
+	Windows: #{user_info[:num_windows]}
+	Fireplace: #{user_info[:fireplace]}
 	"
 # asks user to correct any wrong information
 puts "Is there anything you would like to change? Type \'none\' to skip"
-puts "type name, age, kids, decor, color, wind, or fire regarding what you want to change."
 #need user to say what bit they wanna change
 change = gets.chomp
+
 puts "what would you like to change that to?"
 new_val = gets.chomp
-user_info[change] = new_val
+user_info[change.downcase] == new_val
 #then whatver input shoulkd swp that symbl after
 
 
 # give a chance to update incorrect info - none to skip it
+puts "
+	Name: #{user_info[:name]}
+	Age: #{user_info[:age]}
+	Kids: #{user_info[:num_kids]}
+	Decor: #{user_info[:decor]}
+	Colors: #{user_info[:colors]}
+	Windows: #{user_info[:num_windows]}
+	Fireplace: #{user_info[:fireplace]}
+	"
