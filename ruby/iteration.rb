c0c9927a66558171b1e0927a32c63b9568ae84ec
Grant Willey <ghwilley@gmail.com>
2016-04-03 17:53:31 -0500

    Completed Releases

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,43 +1,50 @@
-# def sports
-#   puts "This is a sports string"
-#   yield("basketball", "football")
-# end
+#Release 1
 
-# sports {|sport1, sport2| puts "#{sport1} and #{sport2} are sports"}
+def sports
+  puts "This is a sports string"
+  yield("basketball", "football")
+end
 
-# hollywood_actors = {
-#   "Denzel Washington" => "John Q",
-#   "Tom Hanks" => "Forrest Gump",
-#   "Leo" => "Jack"
-# }
+sports {|sport1, sport2| puts "#{sport1} and #{sport2} are sports"}
 
-# movie_genres = ["comedy", "drama", "action", "biopic"]
+hollywood_actors = {
+  "Denzel Washington" => "John Q",
+  "Tom Hanks" => "Forrest Gump",
+  "Leo" => "Jack"
+}
 
-# hollywood_actors.each do |actor, character|
-#   puts "#{actor} is best known for his character #{character}"
-# end
+movie_genres = ["comedy", "drama", "action", "biopic"]
 
-# p hollywood_actors
+hollywood_actors.each do |actor, character|
+  puts "#{actor} is best known for his character #{character}"
+end
 
-# p movie_genres
+p hollywood_actors
 
-# movie_genres.map! do |genre|
-#   genre.upcase
-# end
+p movie_genres
 
-# p movie_genres
+movie_genres.map! do |genre|
+  genre.upcase
+end
+
+p movie_genres
+
+#Release 2
 
 letter_array = ["a", "b", "c", "d"]
 
 num_hash = {1 => "one", 2=> "two", 3 => "three"}
 
+#method that iterates through items deleting under condition
+p num_hash.delete_if {|digit, strng| digit.even?}
 
-# p num_hash.delete_if {|digit, strng| digit.even?}
+#method that filters data structure for items that do satisfy condition
+p letter_array.keep_if {|letter| letter =="b" }
 
-# p letter_array.keep_if {|letter| letter =="b" }
+#method that does the same as line 37
+p num_hash.select {|digit, strng| digit > 1}
 
-# p num_hash.select {|digit, strng| digit > 1}
+p num_hash.reject {|digit, strng| digit > 1}
 
-# p num_hash.reject {|digit, strng| digit > 1}
-
+# method that removes until condition is false and then stops
 p letter_array.take_while {|letter| letter != "c"}
