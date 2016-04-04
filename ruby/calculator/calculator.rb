28083a3418a94b4b2d8c92207a2dcb1d6e1f1c2c
Grant Willey <ghwilley@gmail.com>
2016-04-04 14:51:51 -0500

    initial commit

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,13 +1,37 @@
-class Calculator
-  def add(x,y)
-    x + y
-  end
-
-  def subtract(x,y)
-    x - y
-  end
-
-  def multiply(x,y)
-    x * y
-  end
+#METHOD CODE
+def calculate(int1,op_str,int2)
+  op_str = op_str.to_s
+  result = int1.send(op_str, int2)
 end
+
+
+#DRIVER CODE
+calc_array = []
+
+def calc_store
+    puts "Please enter a calculation, type \'done\' to exit"
+    calculation = nil
+      while calculation != 'done'
+      calculation = gets.chomp
+        if calculation.length != 3
+        puts'error'
+        elsif calculation != 'done'
+          calculation.split('')
+          int1 = calculation[0].to_i
+          op_str = calculation[1].to_s
+          int2 = calculation[2].to_i
+
+          calculate(int1,op_str,int2)
+          yield(int1,op_str,int2,calculate(int1,op_str,int2))
+        end
+      end
+
+end
+
+calc_store { |int1,op_str,int2,result| 
+    calc_array.push("#{int1} #{op_str} #{int2} = #{result}")
+  }
+
+puts "calculations performed:"
+
+p calc_array
---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#SPECIES ----------
+#Puppy
+
+#CHARACTERISTICS --------
+#Legs: 4
+#Fur color: varies
+#Eyes: 2
+#Name: varies
+#Ears: 2
+#Tails: 1
+
+
+#BEHAVIOR ------------
+#Run
+#Retrieve
+#Bark
