65d0a2f555134e1ed06d7d6ed22c8d6feb85421c
Grant Willey <ghwilley@gmail.com>
2016-04-05 23:53:49 -0500

    completed release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class Santa
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+		"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@age = 0
 
   def speak
     puts "Ho, ho, ho! Haaaaappy holidays!"
@@ -8,15 +11,27 @@ class Santa
     puts "That was a good #{cookie_type}"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
+  	@gender = gender
+  	@ethnicity = ethnicity
     puts "Initializing Santa instance..."
   end
 
 end
 
-st_nick = Santa.new
+# st_nick = Santa.new
+
+# st_nick.speak
+
+# st_nick.eat_milk_and_cookies("oatmeal")
+
+santas = []
+santa_ethnic = ["Cambodian", "Lithuanian", "Dutch", "Danish"]
+santa_gender = ["male", "female", "dutch", "female"]
+
+santa_gender.length.times do |i|
+	santas << Santa.new(santa_gender[i], santa_ethnic[i])
+end
 
 
-st_nick.speak
-
-st_nick.eat_milk_and_cookies("oatmeal")
+p santas
