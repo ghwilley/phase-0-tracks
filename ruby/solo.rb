e7fb775e1455a298bd8c043592956609858554be
Grant Willey <ghwilley@gmail.com>
2016-04-07 11:51:28 -0500

    initial commit

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,23 +1,47 @@
-# Morty
-# ATTRIBUTES : Age, hp, xp_stored, attack, defense, weight, height
+# Morty (like from Rick and Morty's Pocket Morty's, a  Pokemon ripoff)
+# ATTRIBUTES : Age, hp, xp_stored, attack, defense
 # METHODS: lvl(xp), status(chk hp - green, yellow, red), take_dmg, deliver dmg
 
 class Morty
 
-	attr_reader :attack, :defense, :weight, :height, :age
-	attr_accessor :xp, :lvl. :status
+	attr_reader :attack, :defense, :hp, :age
+	attr_accessor :xp, :lvl, :status
 
 def initialize
+	@hp = 30
 	@xp = 0
 	@lxl = 0
 	@attack = 15
 	@defense = 14
-	@weight = 40
-	@height = 55
 	@age = 5
-	
+	@status = ["green", "yellow", "red"]
+
 end
 
-stats = ["green", "yellow", "red"]
+def damage_calc
+	dmg = rand(0 - 15)
+	@hp -= dmg
+	@hp
+end
 
-@status = stats[0]
+def stat_calc
+	if @hp <= 10
+		stat_calc = @status[2]
+	elsif @hp <= 20
+		stat_calc = @status[1]
+	else
+		stat_calc = @status[0]
+	end
+end
+
+def attack_cal
+end
+end
+
+#DRIVER CODE --------------
+the_one_true_morty = Morty.new
+
+#Takes damage and calcs hp
+p the_one_true_morty.damage_calc
+#calculates status
+p the_one_true_morty.stat_calc
